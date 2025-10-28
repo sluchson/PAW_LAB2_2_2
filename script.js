e96ea0f5eae01f39f732dd06717605992edb7e97
Maciej Pereślucha <147943628+sluchson@users.noreply.github.com>
2025-10-28 08:36:31 +0000

    Com2_2.1

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-(function () {
+(function() {
   const example = document.getElementById('example')
   const cw1 = document.getElementById('cw1')
   const cw2 = document.getElementById('cw2')
   const cw3 = document.getElementById('cw3')
   const answer = document.getElementById('answer')
 
-  example.addEventListener("click", function () {
+  example.addEventListener("click", function() {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
       .then(array => {
@@ -14,15 +14,32 @@
       })
   })
 
-  cw1.addEventListener("click", function () {
+  cw1.addEventListener("click", function() {
     //TODO
   })
 
-  cw2.addEventListener("click", function () {
-    //TODO
-  })
+  cw2.addEventListener("click", function() {
+    answer.textContent = "Loading...";
 
-  cw3.addEventListener("click", function () {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(posts => {
+        const htmlPosts = posts.map(post => {
+          return `
+            <div class="post">
+              <h3>${post.title}</h3>
+              <p>${post.body}</p>
+            </div>
+          `;
+        });
+
+        setTimeout(() => {
+          answer.innerHTML = htmlPosts.join('');
+        }, 1000);
+      })
+  });
+
+  cw3.addEventListener("click", function() {
     //TODO
   })
 
